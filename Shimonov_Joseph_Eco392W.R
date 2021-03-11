769d23e8f04a934636dda7ca42e0487d66753d07
JShimonov <jshimonov@gmail.com>
2021-03-11 15:17:32 -0500

    stats for returns over time

--- Shimonov_Joseph_Eco392W.R
+++ Shimonov_Joseph_Eco392W.R
@@ -31,17 +31,23 @@ summary(returns_data)
 # -----------------------------------------------------------------------
 
 d_mat = as.Date(returns_data$Date, format = "%y/%m/%d")
-r_mat = c(returns_data$`Bloomberg Barclays US Treasury Index Returns`, 
+
+r_mat = cbind(returns_data$`Bloomberg Barclays US Treasury Index Returns`, 
           returns_data$`Bloomberg Barclays US Corporate High Yield Bond Index Returns`,
           returns_data$`Bloomberg Barclays US Corporate Bond Index Returns`)
 
-r_xts <- xts(r_mat, order.by=d_mat)
+r_xts <- xts(r_mat, order.by = d_mat)
 
-ga_spe <- ugarchspec(mean.model = list(armaOrder = c(1,1)), variance.model = list(garchOrder = c(1,1), model = "sGARCH"), distribution.model = "norm")
+ga_spe <- ugarchspec(mean.model = list(armaOrder = c(1,1)), 
+                     variance.model = list(garchOrder = c(1,1), model = "sGARCH"), 
+                     distribution.model = "norm")
+
 ga_fit <- ugarchfit(ga_spe, data = r_xts, log = TRUE)
 ga_fit
 
-dcc_spe <- dccspec(uspec = multispec( replicate(ncol(r_xts), ga_spe) ), dccOrder = c(1,1), distribution = "mvnorm") 
+dcc_spe <- dccspec(uspec = multispec(replicate(ncol(r_xts), ga_spe)), 
+                   dccOrder = c(1,1), 
+                   distribution = "mvnorm") 
+
 dcc_fit <- dccfit(dcc_spe, data = r_xts)
-
-
+dcc_fit

f3c8d8ba619a1f2ae42e04b6d5c40eed1e831218
JShimonov <jshimonov@gmail.com>
2021-03-14 15:45:33 -0400

    added images

--- Shimonov_Joseph_Eco392W.R
+++ Shimonov_Joseph_Eco392W.R
@@ -29,12 +29,26 @@ colnames(returns_data) <- c("Date",
 
 d_mat = as.Date(returns_data$Date, format = "%y/%m/%d")
 
+# Multi returns
 r_mat = cbind(returns_data$`Bloomberg Barclays US Treasury Index Returns`, 
           returns_data$`Bloomberg Barclays US Corporate High Yield Bond Index Returns`,
           returns_data$`Bloomberg Barclays US Corporate Bond Index Returns`)
 
 r_xts <- xts(r_mat, order.by = d_mat)
-plot(r_xts)
+
+# Individual returns
+r_mat_treasury = returns_data$`Bloomberg Barclays US Treasury Index Returns`
+r_mat_HY = returns_data$`Bloomberg Barclays US Corporate High Yield Bond Index Returns`
+r_mat_IG = returns_data$`Bloomberg Barclays US Corporate Bond Index Returns`
+
+r_xts_treasury <- xts(r_mat_treasury, order.by = d_mat)
+r_xts_HY <- xts(r_mat_HY, order.by = d_mat)
+r_xts_IG <- xts(r_mat_IG, order.by = d_mat)
+
+# plot
+plot(r_xts_treasury)
+plot(r_xts_HY)
+plot(r_xts_IG)
 
 ga_spe <- ugarchspec(mean.model = list(armaOrder = c(1,1)), 
                      variance.model = list(garchOrder = c(1,1), model = "sGARCH"), 

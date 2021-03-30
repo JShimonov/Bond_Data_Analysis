1216fa80164d5bf338ba5d538c8c259e59342b3a
JShimonov <jshimonov@gmail.com>
2021-03-30 13:55:35 -0400

    changed data

--- Shimonov_Joseph_Eco392W.R
+++ Shimonov_Joseph_Eco392W.R
@@ -12,43 +12,56 @@ lapply(
 setwd("~/Desktop/ECON392W")
 
 # import datasets
-returns_data <- read_excel("Bond Data/bloomberg_bond_data.xlsx")
+#returns_data <- read_excel("Bond Data/bloomberg_bond_data.xlsx")
+returns_data <- read_excel('Bond Data/Data.xlsx')
 
 # convert datasets to dataframes
 returns_data <- as.data.frame(returns_data)
 
 # remove rows from yield_data where the row values are empty
-returns_data <- returns_data[!(is.na(returns_data$`Bloomberg Barclays US Treasury Index Returns`) | is.na(returns_data$`Bloomberg Barclays US Corporate High Yield Bond Index Returns`) | is.na(returns_data$`Bloomberg Barclays US Corporate Bond Index Returns`)), ]
-returns_data <- cbind(returns_data[0], returns_data$`Date`, returns_data$`Bloomberg Barclays US Treasury Index Returns`, returns_data$`Bloomberg Barclays US Corporate High Yield Bond Index Returns`, returns_data$`Bloomberg Barclays US Corporate Bond Index Returns`)
+#returns_data <- returns_data[!(is.na(returns_data$`Bloomberg Barclays US Treasury Index Returns`) | is.na(returns_data$`Bloomberg Barclays US Corporate High Yield Bond Index Returns`) | is.na(returns_data$`Bloomberg Barclays US Corporate Bond Index Returns`)), ]
+#returns_data <- cbind(returns_data[0], returns_data$`Date`, returns_data$`Bloomberg Barclays US Treasury Index Returns`, returns_data$`Bloomberg Barclays US Corporate High Yield Bond Index Returns`, returns_data$`Bloomberg Barclays US Corporate Bond Index Returns`)
+
+returns_data <- returns_data[!(is.na(returns_data$`HY Returns`) | is.na(returns_data$`IG Returns`) | is.na(returns_data$`Treasury Returns`)), ]
+returns_data <- cbind(returns_data[0], returns_data$`Date`, returns_data$`HY Returns`, returns_data$`IG Returns`, returns_data$`Treasury Returns`)
+
 colnames(returns_data) <- c("Date", 
-                            "Bloomberg Barclays US Treasury Index Returns",
-                            "Bloomberg Barclays US Corporate High Yield Bond Index Returns",
-                            "Bloomberg Barclays US Corporate Bond Index Returns")
+                            "HY Returns",
+                            "IG Returns",
+                            "Treasury Returns")
 
 # -----------------------------------------------------------------------
 
 d_mat = as.Date(returns_data$Date, format = "%y/%m/%d")
 
 # Multi returns
-r_mat = cbind(returns_data$`Bloomberg Barclays US Corporate High Yield Bond Index Returns`,
-              returns_data$`Bloomberg Barclays US Corporate Bond Index Returns`,
-              returns_data$`Bloomberg Barclays US Treasury Index Returns`)
+#r_mat = cbind(returns_data$`Bloomberg Barclays US Corporate High Yield Bond Index Returns`,
+#              returns_data$`Bloomberg Barclays US Corporate Bond Index Returns`,
+#              returns_data$`Bloomberg Barclays US Treasury Index Returns`)
+
+r_mat = cbind(returns_data$`HY Returns`,
+              returns_data$`IG Returns`,
+              returns_data$`Treasury Returns`)
+
 
 r_xts <- xts(r_mat, order.by = d_mat)
+r_xts_df <- as.data.frame(r_xts)
+colnames(r_xts_df) <- c("HY", "IG", "Treasury")
+r_xts <- as.xts(r_xts_df)
 
 # Individual returns
-r_mat_treasury = returns_data$`Bloomberg Barclays US Treasury Index Returns`
-r_mat_HY = returns_data$`Bloomberg Barclays US Corporate High Yield Bond Index Returns`
-r_mat_IG = returns_data$`Bloomberg Barclays US Corporate Bond Index Returns`
+#r_mat_treasury = returns_data$`Bloomberg Barclays US Treasury Index Returns`
+#r_mat_HY = returns_data$`Bloomberg Barclays US Corporate High Yield Bond Index Returns`
+#r_mat_IG = returns_data$`Bloomberg Barclays US Corporate Bond Index Returns`
 
-r_xts_treasury <- xts(r_mat_treasury, order.by = d_mat)
-r_xts_HY <- xts(r_mat_HY, order.by = d_mat)
-r_xts_IG <- xts(r_mat_IG, order.by = d_mat)
+#r_xts_treasury <- xts(r_mat_treasury, order.by = d_mat)
+#r_xts_HY <- xts(r_mat_HY, order.by = d_mat)
+#r_xts_IG <- xts(r_mat_IG, order.by = d_mat)
 
 # plot
-plot(r_xts_treasury)
-plot(r_xts_HY)
-plot(r_xts_IG)
+#plot(r_xts_treasury)
+#plot(r_xts_HY)
+#plot(r_xts_IG)
 
 ga_spe <- ugarchspec(mean.model = list(armaOrder = c(1,1)), 
                      variance.model = list(garchOrder = c(1,1), model = "sGARCH"), 
